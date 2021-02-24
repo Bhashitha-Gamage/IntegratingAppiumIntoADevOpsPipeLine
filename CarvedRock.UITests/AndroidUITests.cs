cb065915d11b5f866ffdfc5e5e4c336bd87b334a
Bhashitha-Gamage <gamage25@gmail.com>
2021-02-24 23:43:15 +0100

    removed impl. waiting

--- CarvedRock.UITests/AndroidUITests.cs
+++ CarvedRock.UITests/AndroidUITests.cs
@@ -110,14 +110,14 @@ namespace CarvedRock.UITests
             AndroidDriver<AppiumWebElement> driver = StartApp();
 
             // Test by adding wait time
-            Thread.Sleep(10000);
+           // Thread.Sleep(10000);
 
             // tap on second item
             var el1 = driver.FindElement(MobileBy.AccessibilityId("Add"));
             el1.Click();
 
             // Test by adding wait time
-            Thread.Sleep(10000);
+           // Thread.Sleep(10000);
 
             var elItemText = driver.FindElement(MobileBy.AccessibilityId("ItemText"));
             elItemText.Clear();
